fe733c94194ca135de0492d3c44b838769fb7442
jonjia <jiawenhuiwar3@163.com>
2024-07-21 13:53:17 +0800

    feat: finish chapter 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: Define a new `Order` type.
+// Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
 //   The product name can't be empty and it can't be longer than 300 bytes.
 //   The quantity must be strictly greater than zero.
@@ -11,3 +11,63 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() || product_name.len() > 300 || quantity == 0 || unit_price == 0 {
+            panic!("Invalid input");
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("Invalid input");
+        }
+
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if quantity == 0 {
+            panic!("Invalid input");
+        }
+
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        if unit_price == 0 {
+            panic!("Invalid input");
+        }
+
+        self.unit_price = unit_price;
+    }
+}
